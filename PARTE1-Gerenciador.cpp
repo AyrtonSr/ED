1aa3d558a52ee232cd24c27bca25b2660d7114c5
AyrtonSr <133722471+AyrtonSr@users.noreply.github.com>
2023-11-16 01:35:01 -0400

    Update PARTE1-Gerenciador.cpp

--- PARTE1-Gerenciador.cpp
+++ PARTE1-Gerenciador.cpp
@@ -4,7 +4,7 @@
 
 using namespace std;
 
-
+// criação da fila
 typedef struct FILA
 {
   int nPacote;
@@ -19,11 +19,11 @@ void gotoxy(int x, int y)
   SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
 }
 
-void enqueue(FILA **inicio, FILA **fim, int pacote){
+void enqueue(FILA **inicio, FILA **fim, int pacote){ // metodo responsavel por acrescentar um novo item na fila
 
-    FILA *novoPacote = new FILA();
+    FILA *novoPacote = new FILA(); // Atribui uma nova região de memória à um ponteiro
 
-    if ((*inicio)==NULL){
+    if ((*inicio)==NULL){ // verificação se ja existe uma fila
         (*inicio)=novoPacote;
         (*fim)=novoPacote;
         novoPacote->nPacote = pacote;
@@ -36,9 +36,9 @@ void enqueue(FILA **inicio, FILA **fim, int pacote){
     }
 }
 
-FILA *dequeue(FILA *&inicio){
+FILA *dequeue(FILA *&inicio){ // metodo responsavel por tirar o item do incio da fila e retornalo para ser exibido
 
-    FILA *aux;
+    FILA *aux; // aux é responsavel por armazenar o item que sera exibido apos esse metodo
     aux=inicio;
     inicio= inicio->proxpacote;
     return aux;
@@ -54,39 +54,47 @@ void telaDeCarregamento(){
     }
     for(int i=20;i<=45;i++){
     gotoxy(i,5);printf("%c", bar2);
-    Sleep(150);
+    Sleep(75);
     }
 }
 
 
 int main (){
+
     FILA *inicio = NULL;
     FILA *fim = NULL;
     int tamanho = 0, pacotes = 50, qtdpacotes = 0, c=1;
 
+    gotoxy(2,2);printf("%c", 254);
     gotoxy(4,2);cout << "INFORME O TAMANHO DO ARQUIVO(EM KB): ";
     cin >> tamanho;
+    // Verificação de quantos pacotes serão necessarios
     if (tamanho%pacotes == 0){
         qtdpacotes = tamanho/pacotes;
     } else{
         qtdpacotes = (tamanho/pacotes)+1;
     }
-    gotoxy(5,2);cout << "Quantidade de pacotes necessarios: "<<qtdpacotes<<endl;
+
+    gotoxy(2,3);printf("%c", 254);
+    gotoxy(4,3);cout << "Quantidade de pacotes necessarios: "<<qtdpacotes<<endl;
     system("pause");
 
+    //Vai acrescentar de acordo com a quantidade de pacotes necessarios
     while(c<=qtdpacotes){
         enqueue(&inicio, &fim, c);
         c++;
     }
     int y=7;
     system("cls");
+
+    //Vai retirar o inicial e exibir como carregado e apaga ele em seguida
     while(inicio!=NULL){
-        FILA *aux;
+        FILA *aux; // aux do tipo FILA que ira receber o intem que esta no inicio da fila
         aux=dequeue(inicio);
         telaDeCarregamento();
         gotoxy(5,y);cout<< "Pacote "<<aux->nPacote<< " Carregado";
         y++;
-        delete(aux);
+        delete(aux);//Deleta o aux para liberar memoria
     }
 
 
